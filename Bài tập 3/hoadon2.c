f2650ad8f14cef4b0d1be975cd158c6ea7e939e5
minhhuy2105 <72544234+minhhuy2105@users.noreply.github.com>
2020-12-10 16:56:36 +0700

    Create hoadon2.c

---
+++ Bài tập 3/hoadon2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    //khai bao bien
+
+    int dongia,soluong,thanhtien;
+    printf("nhap don gia mat hang");
+    
+    scanf ("%d",&dongia);
+    printf("nhap so luong mat hang");
+    scanf ("%f",&soluong);
+    //dieu kien
+    if("soluong >= 5", soluong);
+}
+    getchar();
+    return 0;
+}    

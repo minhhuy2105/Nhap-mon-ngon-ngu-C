0de8c24411ed533b75263ad7224ea2814cb01b2c
minhhuy2105 <72544234+minhhuy2105@users.noreply.github.com>
2020-12-24 14:40:58 +0700

    Create xinchao .c

---
+++ Bài tập 3/xinchao .c
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+ int main() {
+     char  []= "xin chao!";
+	 cout << "xin chao" << endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	 cout << "xin chao"<< endl;
+	     	
+return 0; 
+	}    	
